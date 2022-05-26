31a5e6ecc4a744acbe69c7aef23d5b806cbb4978
linhduongpscd <linhduong.pscd@gmail.com>
2022-05-26 10:17:55 +0700

    update server

--- server.js
+++ server.js
@@ -2,14 +2,8 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('data/db.json')
-const middlewares = jsonServer.defaults()
-
 const app = express()
-app.use(middlewares)
-app.use(router)
+
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
